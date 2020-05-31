bbc388effb506c33a800a7948c56f82bb71c3d9c
Hugo <hugoandres272@gmail.com>
2020-05-31 18:05:03 -0500

    Primer prueba funcionamiento producción

--- javascript/cod_menu.js
+++ javascript/cod_menu.js
@@ -1,12 +1,12 @@
-var contador = 1;
 function iniciar() {
   var boton_menu = document.getElementById('menu_icon');
   boton_menu.addEventListener("click",volver_visual);
 }
+var contador = 1;
 function volver_visual() {
   var menu_li = document.getElementById('menu-cabecera');
 
-  if(contador == 1){
+  if(contador == 1 && screen.width<1000){
     menu_li.style.top='174px';
     menu_li.style.left='0';
     contador = 0;
@@ -15,4 +15,24 @@ function volver_visual() {
     menu_li.style.left='-110%';
   }
 }
+
+function modificacionUno() {
+  if(screen.width<1000){
+    var boton_press1 = document.querySelector("[class='menu-item btn1']");
+    var boton_press2 = document.querySelector("[class='menu-item btn2']");
+    var boton_press3 = document.querySelector("[class='menu-item btn3']");
+    var boton_press5 = document.querySelector("[class='menu-item btn5']");
+    boton_press1.addEventListener("click",modificacionMostrarBoton);
+    boton_press2.addEventListener("click",modificacionMostrarBoton);
+    boton_press3.addEventListener("click",modificacionMostrarBoton);
+    boton_press5.addEventListener("click",modificacionMostrarBoton);
+  }
+}
+function modificacionMostrarBoton(){
+  console.log(screen.width);
+  var menu_li = document.getElementById('menu-cabecera');
+  menu_li.style.left='-110%';
+  contador=1;
+}
 window.addEventListener("load", iniciar);
+window.addEventListener("click",modificacionUno);
--- javascript/menu-fixed.js
+++ javascript/menu-fixed.js
@@ -3,6 +3,7 @@ window.addEventListener('scroll',change);
 function change (){
   const menu = document.getElementById('menu-cabecera');
   const modif = document.querySelector('[class="ul-menu-principal"]')
+
   if(document.scrollingElement.scrollTop>30){
     menu.classList.add('menu-fixed');
     modif.style.margin="0%";
@@ -10,4 +11,21 @@ function change (){
     menu.classList.remove('menu-fixed');
     modif.style.margin="30px 0px";
   }
+
+  // // Capturar los valores de marginTop de header-inicio-des
+  // const elementHeader = document.getElementById('header-inicio-des');
+  // let elementStyle = window.getComputedStyle(elementHeader);
+  // let elementMarginTop = elementStyle.getPropertyValue('margin-top');
+  // let valueTop = valueMarginTop(elementMarginTop);
+  // if(document.scrollingElement.scrollTop>29 && document.scrollingElement.scrollTop<31){
+  //   elementHeader.style.marginTop=(parseFloat(valueTop)+100)+"px";
+  // }
 }
+//
+// function valueMarginTop(cadena){
+//   var cadAux = "";
+//   for (var i = 0; i < (cadena.length-2); i++) {
+//     cadAux = cadAux+cadena[i];
+//   }
+//   return cadAux;
+// }
